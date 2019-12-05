dc4880970f17a3bf04da53b59807aef68d0eae46
Flyewzz <dentalon599@gmail.com>
2019-12-05 14:17:58 +0300

    Added pagination, method validation, pagination handler, updated go version, added new tests, auto-sort map, new method for taskController (pagination)

--- handlers.go
+++ handlers.go
@@ -7,6 +7,7 @@ import (
 func ConfigureHandlers(r *mux.Router) {
 	r.HandleFunc("/request", RequestHandler).Methods("GET")
 	r.HandleFunc("/tasks", AllTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/page/{number}", PageTasksHandler).Methods("GET")
 	r.HandleFunc("/tasks/{id}", TaskIdHandler).Methods("GET")
 	r.HandleFunc("/tasks/{id}", RemoveTaskIdHandler).Methods("DELETE")
 }
