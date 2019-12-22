8c42e745ed63a641fe9d8f5aaaeb513287d1a038
Flyewzz <dentalon599@gmail.com>
2019-12-22 18:35:46 +0300

    Added test SaveResult

--- workers/worker_test.go
+++ workers/worker_test.go
@@ -32,20 +32,27 @@ func (ex *FailExecutor) Execute(client *http.Client, task *models.Task) (*models
 	return nil, errors.New("Some error... :(")
 }
 
-type MockStoreController struct{}
+type MockStoreController struct {
+	last *models.Request
+}
 
 func NewMockStoreController() *MockStoreController {
 	return &MockStoreController{}
 }
 
-func (st *MockStoreController) Add(request *models.Request) int { return 0 }
+func (st *MockStoreController) Add(request *models.Request) int { st.last = request; return 0 }
 func (st *MockStoreController) GetAll() []models.Request        { return []models.Request{} }
 func (st *MockStoreController) GetByPage(page int) ([]models.Request, error) {
 	return []models.Request{}, nil
 }
-func (st *MockStoreController) GetById(id int) (*models.Request, error) { return &models.Request{}, nil }
-func (st *MockStoreController) RemoveById(id int) error                 { return nil }
-func (st *MockStoreController) RemoveAll()                              {}
+func (st *MockStoreController) GetById(id int) (*models.Request, error) {
+	if id == 0 {
+		return st.last, nil
+	}
+	return nil, errors.New("Incorrect index")
+}
+func (st *MockStoreController) RemoveById(id int) error { return nil }
+func (st *MockStoreController) RemoveAll()              {}
 
 func TestNewWorker(t *testing.T) {
 	type args struct {
@@ -122,6 +129,7 @@ func TestWorker_SendResult(t *testing.T) {
 }
 
 func TestWorker_SaveResult(t *testing.T) {
+	jobCh := make(chan workerModels.Job)
 	type args struct {
 		job    *workerModels.Job
 		result *models.Result
@@ -130,12 +138,78 @@ func TestWorker_SaveResult(t *testing.T) {
 		name string
 		w    *Worker
 		args args
+		want *models.Request
 	}{
-		// TODO: Add test cases.
+		{
+			name: "get google",
+			w: NewWorker(0, jobCh, NewMockSuccessExecutor(),
+				NewMockStoreController()),
+			args: args{
+				job: &workerModels.Job{
+					Task: &models.Task{
+						Method: "GET",
+						Url:    "https://google.ru",
+					},
+				},
+				result: &models.Result{
+					Response: models.NewResponse("200 OK", "Header1: header1",
+						"body", 4),
+					Error: nil,
+				},
+			},
+			want: &models.Request{
+				Task: &models.Task{
+					Method: "GET",
+					Url:    "https://google.ru",
+				},
+				Response: models.NewResponse("200 OK", "Header1: header1",
+					"body", 4),
+			},
+		},
+		{
+			name: "post yandex",
+			w: NewWorker(0, jobCh, NewMockSuccessExecutor(),
+				NewMockStoreController()),
+			args: args{
+				job: &workerModels.Job{
+					Task: &models.Task{
+						Method: "POST",
+						Url:    "https://yandex.ru",
+					},
+				},
+				result: &models.Result{
+					Response: models.NewResponse("200 OK", "Header1: header1",
+						"body", 4),
+					Error: nil,
+				},
+			},
+			want: &models.Request{
+				Task: &models.Task{
+					Method: "POST",
+					Url:    "https://yandex.ru",
+				},
+				Response: models.NewResponse("200 OK", "Header1: header1",
+					"body", 4),
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.w.SaveResult(tt.args.job, tt.args.result)
+			stored, err := tt.w.storeController.GetById(0)
+			if err != nil {
+				t.Error("Unexpected error with getById(0)")
+			}
+			if !reflect.DeepEqual(tt.want, stored) {
+				t.Errorf("Test '%s' was failed. "+
+					"stored task: method: %s, url: %s; "+
+					"actual task: method: %s, url: %s\n"+
+					"stored response: status: %s, headers: %s, body: %s, Content-Length: %d\n"+
+					"actual response: status: %s, headers: %s, body: %s, Content-Length: %d\n", tt.name,
+					stored.Task.Method, stored.Task.Url, tt.want.Task.Method, tt.want.Task.Url,
+					stored.Response.Status, stored.Response.Headers, stored.Response.Body, stored.Response.ContentLength,
+					tt.want.Response.Status, tt.want.Response.Headers, tt.want.Response.Body, tt.want.Response.ContentLength)
+			}
 		})
 	}
 }
